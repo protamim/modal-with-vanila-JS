c27fa29f719bba4afd52355d8cbd2cc7fb5d0235
Protamim <s163.tamim@gmail.com>
2022-05-02 11:57:05 +0600

    Product wishlish modal

---
+++ index.js
@@ -0,0 +1,28 @@
+const openModalBtn = document.querySelectorAll('.open-modal')
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeModalBtn = document.querySelector('.close-modal')
+
+const openModal = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for(let i = 0; i < openModalBtn.length; i++) {
+    openModalBtn[i].addEventListener('click', openModal)
+
+    closeModalBtn.addEventListener('click', closeModal)
+
+    overlay.addEventListener('click', closeModal)
+
+    document.addEventListener('keydown', function (e) {
+        console.log(e.key)
+        if(e.key === 'Escape') {
+            closeModal()
+        }
+    })
+}
